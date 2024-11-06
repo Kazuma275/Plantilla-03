866fe07758a055da8a3253d90d6e306589b31579
Kazuma275 <sergioramosalarcon@gmail.com>
2024-11-06 11:04:07 +0100

    plantilla 03

---
+++ js/function.js
@@ -0,0 +1,8 @@
+/******************** fuction.js de index.html ********************/
+
+"use strict"
+
+function mostrar(resultado) {
+    console.log(`Estoy mostrando el resultado del ${resultado.getAttribute('name')}`);
+    resultado.innerHTML = `Estoy mostrando el resultado del ${resultado.getAttribute('name')}`;
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+/******************** función script.js de index.html ********************/
+
+"use strict"
+
+const boton = document.getElementById("boton");
+const resultado = document.getElementById("resultado");
+boton.addEventListener("click", function () {
+    mostrar(resultado);
+});
